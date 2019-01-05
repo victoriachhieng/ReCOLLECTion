a6071f626c3912df4dee5da972e055c3ac5ec911
Victoria Chhieng <victoria_chhieng@hotmail.com>
2019-01-04 20:52:00 -0600

    add Profiles and CSS to it, create Routes and Links for Profiles, add dummy data

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 import LogOutButton from '../LogOutButton/LogOutButton';
 import './Nav.css';
 
-const Nav = (props) => (
+const Nav = props => (
   <div className="nav">
     <Link to="/home">
       <h2 className="nav-title">ReCOLLECTion</h2>
@@ -14,7 +14,7 @@ const Nav = (props) => (
         {/* Show this link if they are logged in or not,
         but call this link 'Home' if they are logged in,
         and call this link 'Login / Register' if they are not */}
-        {props.user.id ? 'Home' : 'Login / Register'}
+        {props.user.id ? "Home" : "Login / Register"}
       </Link>
       {/* Show the link to the info page and the logout button if the user is logged in */}
       {props.user.id && (
@@ -22,7 +22,10 @@ const Nav = (props) => (
           <Link className="nav-link" to="/info">
             Info Page
           </Link>
-          <LogOutButton className="nav-link"/>
+          <Link className="nav-link" to="/profiles">
+            Profiles
+          </Link>
+          <LogOutButton className="nav-link" />
         </>
       )}
       {/* Always show this link since the about page is not protected */}
